bcd0340bc2e06f2a3c0e3732dfec9f441d11eafc
Wint Hnin <wintyhnin@email.wustl.edu>
2019-01-01 00:51:45 -0600

    trying out bs4 with a sample webpage

--- scrapper.py
+++ scrapper.py
@@ -1,3 +1,13 @@
 """
     Scrapes image data from websites        
 """ 
+
+from bs4 import BeautifulSoup
+
+with open("data/bags.htm") as fp:
+	soup = BeautifulSoup(fp, "lxml")
+
+image_tags = soup.find_all('img')
+
+for img in image_tags:
+	print(img['src'] + "\n")
