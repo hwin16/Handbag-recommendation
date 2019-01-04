2185c88ce58a5d57022118b4421e993ed3ca2729
Wint Hnin <wintyhnin@email.wustl.edu>
2019-01-03 19:11:37 -0600

    download *all* images in the webpage

--- scrapper.py
+++ scrapper.py
@@ -1,15 +1,22 @@
 """
     Scrapes image data from websites        
 """ 
-import requests
+import urllib.request
 from bs4 import BeautifulSoup
 
 url = "https://shop.nordstrom.com/c/womens-handbags-and-wallets?origin=topnav&breadcrumb=Home%2fWomen%2fHandbags"
-response = requests.get(url)
+response = urllib.request.urlopen(url)
 
-soup = BeautifulSoup(response.content, "lxml")
+soup = BeautifulSoup(response, "lxml")
 
 image_tags = soup.find_all('img')
 
 for img in image_tags:
-	print(img['src'] + "\n")
+	pic_link = img['src']
+	pic_name = pic_link[:pic_link.index('?')]
+	pic_name = pic_name.split('/')[-1]
+
+	print(pic_link, pic_name)
+	filename, headers = urllib.request.urlretrieve(pic_link, 'data/' + pic_name)
+
+	print(filename)
